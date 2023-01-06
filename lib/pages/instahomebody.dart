f084dc582c8395a0fd2477625338150ddf64d21f
Amu <Amansis.g2@gmail.com>
2023-01-07 02:57:45 +0300

    stories edit

--- lib/pages/instahomebody.dart
+++ lib/pages/instahomebody.dart
@@ -14,8 +14,18 @@ class instahomebody extends StatelessWidget {
         children: <Widget>[
           
           Flexible(
+            
             child: storylist(),
           ),
+           Container(
+              width:360,
+              height:300,
+              decoration: BoxDecoration(
+              
+                  image:
+                      DecorationImage( image: NetworkImage("https://www.publicbooks.org/wp-content/uploads/2019/11/joel-mott-LaK153ghdig-unsplash-scaled-e1574787737429-810x625.jpg"))),
+            
+            ),
         ],
       ),
     );
