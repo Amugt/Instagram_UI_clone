53b19c584fba85c3363482363cb58e623a2b909d
Amu <Amansis.g2@gmail.com>
2023-01-14 19:58:48 +0300

    some edit

--- lib/pages/postlist.dart
+++ lib/pages/postlist.dart
@@ -5,21 +5,6 @@ class postlist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      scrollDirection: Axis.vertical,
-      itemBuilder: (context, index) => Container(
-        child: Container(
-          width: 350,
-          height: 300,
-          margin: EdgeInsets.only(bottom: 8),
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlb4RdVNhWU7GYqtTGlQTThy4V57WVAGWR0w&usqp=CAU"))),
-        ),
-      ),
-    );
+    return Container();
   }
 }
